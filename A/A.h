4b6efd7e2de63f61c55abe8b8240934f23ae633a
Ho, Derrick <Dho@webmd.net>
2016-06-04 23:10:08 -0400

    Initial Commit

---
+++ A/A.h
@@ -0,0 +1,19 @@
+//
+//  A.h
+//  A
+//
+//  Created by Ho, Derrick on 6/4/16.
+//  Copyright © 2016 Ho, Derrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for A.
+FOUNDATION_EXPORT double AVersionNumber;
+
+//! Project version string for A.
+FOUNDATION_EXPORT const unsigned char AVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <A/PublicHeader.h>
+
+
