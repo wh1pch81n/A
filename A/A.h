194ff3e53c8e02c717964bcace63e2b14ffd84bc
Derrick Ho <derrickho328@gmail.com>
2016-06-05 00:42:57 -0400

    chaining

--- A/A.h
+++ A/A.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char AVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <A/PublicHeader.h>
 
 
+#import <A/AA.h>
