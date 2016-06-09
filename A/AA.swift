06988040e9a348efa96d3408c2952cc460e1e858
Derrick Ho <derrickho328@gmail.com>
2016-06-09 00:46:44 -0400

    no message

---
+++ A/AA.swift
@@ -0,0 +1,42 @@
+//
+//  AA.swift
+//  A
+//
+//  Created by Derrick  Ho on 6/8/16.
+//  Copyright © 2016 Ho, Derrick. All rights reserved.
+//
+
+//public protocol ei {
+//	var sayIt: String { get }
+//}
+
+public extension NSObject {
+	public subscript (key: String) -> AnyObject? {
+		get {
+			return valueForKey(key)
+		}
+		set {
+			setValue(newValue, forKey: key)
+		}
+	}
+	
+}
+
+extension AA {
+	public func thisIsASwiftMethod() {
+		print("This is a Swift method extending an objective-c class")
+	}
+	public override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		print("viewdidappear on the swift side of the moon")
+	}
+	
+	public func sayIt() -> String? {
+		return self["_sayIt"] as! String?
+		//return valueForKey("_sayIt") as! String?
+	}
+	public func setSayIt(s: String?) {
+		self["_sayIt"] = s
+		//setValue(s, forKey: "_sayIt")
+	}
+}
